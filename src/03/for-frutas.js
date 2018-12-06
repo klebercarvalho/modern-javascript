eb0cceef9b525618c74d196957f4cf9a08395c1c
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-12-06 18:31:50 -0200

    for

---
+++ src/03/for-frutas.js
@@ -0,0 +1,4 @@
+const frutas = ['abacaxi', 'maça', 'uva'];
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i]);
+}
---
+++ src/03/for-nomes.js
@@ -0,0 +1,5 @@
+// for
+const nomes = ['maria', 'jose', 'joao'];
+for (let i = 0; i < nomes.length; i++) {
+  console.log(nomes[i]);
+}
