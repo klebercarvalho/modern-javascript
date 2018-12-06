b38dba47201c18a59c3d3cad60829cf982a3da58
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-12-06 20:15:52 -0200

    map

---
+++ src/03/02-map/01-map.js
@@ -0,0 +1,9 @@
+const numeros = [1, 2, 3];
+const dobro = [];
+
+for (let i = 0; i < numeros.length; i++) {
+  dobro.push(numeros[i] * 2);
+}
+
+console.log(numeros); // [1,2,3]
+console.log(dobro); // [2,4,6]
---
+++ src/03/02-map/02-map.js
@@ -0,0 +1,5 @@
+const numeros = [1, 2, 3];
+const dobro = numeros.map(numero => numero * 2);
+
+console.log(numeros); // [1,2,3]
+console.log(dobro); // [2,4,6]
